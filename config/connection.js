8f95631772fcdd340adbb681e7de5084a297a4ca
Mohammad Abbasi <mxabbasi@gmail.com>
2024-05-05 20:07:01 -0700

    update connection.js

--- config/connection.js
+++ config/connection.js
@@ -8,8 +8,9 @@ const sequelize = process.env.DB_URL
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
+      host: '127.0.0.1',
       dialect: 'postgres',
+      port: 5432,
     }
   );
 
